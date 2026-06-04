838db18d8ba0d477d41a640d53b9a2ae121348ca
iyad-f <128908811+iyad-f@users.noreply.github.com>
2026-06-04 16:44:28 +0530

    feat: add echo TCP server

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
 // SPDX-FileCopyrightText: 2026 Iyad
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {
-    println!("Hello, world!");
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+};
+
+fn main() -> std::io::Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6380")?;
+    loop {
+        let (mut stream, _) = listener.accept()?;
+        let mut buf: [u8; 512] = [0; 512];
+
+        loop {
+            let n = stream.read(&mut buf)?;
+            if n == 0 {
+                break;
+            }
+            stream.write_all(&buf[..n])?;
+        }
+    }
 }
